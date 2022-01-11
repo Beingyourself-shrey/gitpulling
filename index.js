fbd501e6a730d71fd8cf7de129c2b6a54f29e5c3
shreyg <shrey.garg@thetrainline.com>
2022-01-11 15:29:58 +0530

    shrey1

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const Express =require('express')
 
 let app=Express();
-app.get('/',(req,res)=>{
+app.get('/',(req,res,next)=>{
 res.send("hello");
+next();
 })
-
+app.use((req,res)=>{
+console.log("hello");
+})
 app.listen(2000);
 
